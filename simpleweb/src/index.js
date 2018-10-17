fc3683aa465aa63545a18894bfca6b9bac9c0068
Gerd Reiss <gerdreiss@jscriptive.com>
2018-10-17 19:10:45 +0200

    refactored to smarter use the docker images caching

--- simpleweb/src/index.js
+++ simpleweb/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<br/><br/><br/><center><h1>Hi there from inside a Docker container!</h1></center>');
+    res.send('<br/><br/><br/><br/><center><h1>Hi there from inside a Docker container!</h1></center>');
 })
 
 app.listen(8080, () => {
