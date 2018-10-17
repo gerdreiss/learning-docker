afb4e1d533540fc977bfcf16c810da379bf6a6b5
Gerd Reiss <gerdreiss@jscriptive.com>
2018-10-17 20:52:11 +0200

    Docker error handling

--- visits/index.js
+++ visits/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
 const client = redis.createClient({
@@ -10,10 +11,29 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+    console.log('executing / request');
     client.get('visits', (err, visits) => {
-        res.send('<br/><br/><br/><center><h1>Number of visits is ' + visits + '</h1></center>');
+        res.send('<br/><center><h1>Hello!</h1></center><br/><center><h2>Number of visits is ' + visits + '</h2></center>');
         client.set('visits', parseInt(visits) + 1);
     });
+});
+
+app.get('/hello', (req, res) => {
+    console.log('executing /hello request');
+    client.get('visits', (err, visits) => {
+        res.send('<br/><br/><br/><center><h1>Hello from inside a Docker container</h1></center>');
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.get('/crash', (req, res) => {
+    console.log('stopping the container without restarting');
+    process.exit(0);
+});
+
+app.get('/crashnrestart', (req, res) => {
+    console.log('stopping and restarting the container after an error');
+    process.exit(1);
 });
 
 app.listen(8081, () => {
