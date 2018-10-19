019545a2ed19647a03878609262b1ad3fd85e2f1
Gerd Reiss <gerdreiss@jscriptive.com>
2018-10-19 20:09:09 +0200

    docker compose

---
+++ frontend/run.sh
@@ -0,0 +1,1 @@
+sudo docker run -p 3000:3000 -v /app/node_modules -v $(pwd):/app $1
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,14 +11,11 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
+          <p>Click <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            rel="noopener noreferrer">here</a> to learn ReactJS</p>
         </header>
       </div>
     );
